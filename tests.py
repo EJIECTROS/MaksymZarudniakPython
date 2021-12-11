1eb526c79c0341544c7f99ee3fd8fead284b892f
Maksym Zarudniak <ejiectros@ukr.net>
2021-12-11 16:43:59 +0200

    HW3

--- tests.py
+++ tests.py
@@ -1,24 +1,12 @@
-var_1 = "volume1"
-var_2 = ["v","o","l","u","m","e","1"]
-
-var_3 = var_1[:-1] + "2"
-
-print(var_3)
-
-
-print(var_1.upper())
-
-var_2.append(345)
-v1 = var_2.pop()
-
-print(var_2, v1)
+def new_func():
+    var = input('fill something')
+    print(f'Function received {var} item')
+for i in range(5):
+    new_func()
 
 
 
 
 
 
-
-
-
-
+new_func()
