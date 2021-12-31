2500380a06975eb247f646dbb0bc63b6d53ca9f7
Maksym Zarudniak <ejiectros@ukr.net>
2021-12-31 12:36:36 +0200

    HW4

--- .idea/Maksym_Zarudniak_Homework_3.py
+++ .idea/Maksym_Zarudniak_Homework_3.py
@@ -7,3 +7,21 @@ for d in n:
     if d.isdigit():  # Если цифра
         s += int(d)
 print(s)  # Сумма цифр
+
+
+n = input('Введите номер: ')  # Ввод
+def vhod(n):
+    if len(n) == 8:  # Проверяла на соответствие Длине введенного текста
+        print('Yes, length number is equal to 8')
+        return n[0:2], n[2], n[3], n[4], n[5], n[6:8]  # Возвращала 6 значений полученных из введенного номера (по 2 буквы и 4
+    else:
+        return False  # Если общее количество символов больше или меньше 8 (во введенных данных) возвращала False
+i = vhod(n)
+print(i)
+
+
+def summm(n):
+    c_count = int(n[2]) + int(n[3]) + int(n[4]) + int(n[5])
+    return c_count
+b = summm(n)
+print(b)
--- PythonsOdessa.py
+++ PythonsOdessa.py
@@ -1,16 +1,6 @@
-a = 65
-if a > 0 and a < 2:
-    print("Младенец")
-elif a>=2 and a<4:
-    print("Малыш")
-elif a>=4 and a<12:
-    print("Ребенок")
-elif a>=12 and a<19:
-    print("Подросток")
-elif a>=19 and a<65:
-    print("Взрослый человек")
-else:
-    a<= 65
-    print("Пожилой человек")
-
-
+k = 10
+z = 0
+while k <= 14:
+    z = z + k
+    k += 1
+print(z)
